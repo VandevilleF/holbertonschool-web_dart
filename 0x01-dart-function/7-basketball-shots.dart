81a65b2515d977473b2dc7c431355b11d0f61eac
VandevilleF <flo.vandeville@gmail.com>
2025-07-11 15:42:09 +0200

    Task 7, local test OK

---
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -0,0 +1,18 @@
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+  int scoreA =
+      teamA['Free throws']! +
+      teamA['2 pointers']! * 2 +
+      teamA['3 pointers']! * 3;
+  int scoreB =
+      teamB['Free throws']! +
+      teamB['2 pointers']! * 2 +
+      teamB['3 pointers']! * 3;
+
+  if (scoreA > scoreB) {
+    return 1;
+  } else if (scoreA < scoreB) {
+    return 2;
+  } else {
+    return 0;
+  }
+}
